255ba79f32e83945895526e360aa9132afedf148
alelarra <alelarrain@gmail.com>
2025-07-03 18:25:34 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cwd } from 'node:process'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      // Expone la variable de entorno al código del cliente de forma segura
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  }
 })
